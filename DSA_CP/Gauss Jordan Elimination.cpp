9e93009f36f4b77ed613232c49c437258b44ff5a
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2023-10-14 19:32:00 +0000

    Update Gauss Jordan Elimination.cpp

--- DSA_CP/Gauss Jordan Elimination.cpp
+++ DSA_CP/Gauss Jordan Elimination.cpp
@@ -1,3 +1,13 @@
+/*
+The Gauss-Jordan method, also known as Gauss-Jordan elimination method is used to solve a system of linear equations and is a modified version of Gauss Elimination Method.
+
+It is similar and simpler than Gauss Elimination Method as we have to perform 2 different process in Gauss Elimination Method i.e. 
+
+    Formation of upper triangular matrix, and 
+    Back substitution
+
+But in case of Gauss-Jordan Elimination Method, we only have to form a reduced row echelon form (diagonal matrix)
+*/
 int gauss (vector < bitset<N> > a, int n, int m, bitset<N> & ans) {
     vector<int> where (m, -1);
     for (int col=0, row=0; col<m && row<n; ++col) {
